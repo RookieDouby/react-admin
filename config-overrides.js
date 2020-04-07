bcc5d6896bba019418c9e43841467d02fa5d933a
张红豆 <zhanghongdaou@encdata.cn>
2020-04-07 17:13:49 +0800

    配置less & antd &自定义主题

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,19 @@
 const {
-    override
+    override,
+    addLessLoader,
+    fixBabelImports,
 } = require('customize-cra')
 
-module.exports = override();
+const modifyVars = require('./lessVars')
+
+module.exports = override(
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars,
+    }),
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    })
+);
