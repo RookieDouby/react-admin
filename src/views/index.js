76cfff6b27e9555fc2767b2f863232d2f524a2f0
张红豆 <zhanghongdaou@encdata.cn>
2020-04-08 15:13:01 +0800

    使用react-loadable懒加载路由

--- src/views/index.js
+++ src/views/index.js
@@ -2,37 +2,37 @@ import Loadable from 'react-loadable'
 // import Loadable from './selfLoadable'
 import { Loading } from '../components'
 
-import Dashboard from './Dashboard'
-import Login from './Login'
-import ArticleList from './Article/ArticleList'
-import ArticleEdit from './Article/ArticleEdit'
-import Settings from './Settings'
-import NotFound from './NotFound'
+// import Dashboard from './Dashboard'
+// import Login from './Login'
+// import ArticleList from './Article/ArticleList'
+// import ArticleEdit from './Article/ArticleEdit'
+// import Settings from './Settings'
+// import NotFound from './NotFound'
 
-// const Dashboard = Loadable({
-//     loader: () => import('./Dashboard'),
-//     loading: Loading,
-// })
-// const Login = Loadable({
-//     loader: () => import('./Login'),
-//     loading: Loading,
-// })
-// const ArticleList = Loadable({
-//     loader: () => import('./Article/ArticleList'),
-//     loading: Loading,
-// })
-// const ArticleEdit = Loadable({
-//     loader: () => import('./Article/ArticleEdit'),
-//     loading: Loading,
-// })
-// const Settings = Loadable({
-//     loader: () => import('./Settings'),
-//     loading: Loading,
-// })
-// const NotFound = Loadable({
-//     loader: () => import('./NotFound'),
-//     loading: Loading,
-// })
+const Dashboard = Loadable({
+    loader: () => import('./Dashboard'),
+    loading: Loading,
+})
+const Login = Loadable({
+    loader: () => import('./Login'),
+    loading: Loading,
+})
+const ArticleList = Loadable({
+    loader: () => import('./Article/ArticleList'),
+    loading: Loading,
+})
+const ArticleEdit = Loadable({
+    loader: () => import('./Article/ArticleEdit'),
+    loading: Loading,
+})
+const Settings = Loadable({
+    loader: () => import('./Settings'),
+    loading: Loading,
+})
+const NotFound = Loadable({
+    loader: () => import('./NotFound'),
+    loading: Loading,
+})
 
 
 
