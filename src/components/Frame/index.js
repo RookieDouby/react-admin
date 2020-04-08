9abe1d29e92bfbc27b4b2d7e3e672b72ee965762
张红豆 <zhanghongdou@enn.cn>
2020-04-08 23:14:39 +0800

    添加antd国际化

--- src/components/Frame/index.js
+++ src/components/Frame/index.js
@@ -48,9 +48,7 @@ class Frame extends Component {
                         <Content
                             className="site-layout-background"
                             style={{
-                                padding: 24,
                                 margin: 0,
-                                minHeight: 280,
                             }}
                         >
                             {
--- src/index.js
+++ src/index.js
@@ -4,35 +4,42 @@ import { render } from 'react-dom'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import { mainRoutes } from './routes'
 
+// 国际化部分
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/es/locale/zh_CN';
+import enUS from 'antd/es/locale/en_US';
+
 import App from './App'
 import './index.less';
 
 render(
-    <Router>
-        {/* <div>这里是公共部分</div> */}
-        <Switch>
-            <Route
-                path="/admin"
-                render={(routerProps) => {
-                    return <App {...routerProps} />
-                }}
-            />
-            {
-                mainRoutes.map(route => {
-                    return (
-                        <Route
-                            key={route.pathname}
-                            path={route.pathname}
-                            render={routerProps => {
-                                return <route.component {...routerProps} />
-                            }}
-                        />
-                    )
-                })
-            }
-            <Redirect to="/admin" from="/" exact />
-            <Redirect to="/404" />
-        </Switch>
-    </Router>,
+    <ConfigProvider locale={zhCN}>
+        <Router>
+            {/* <div>这里是公共部分</div> */}
+            <Switch>
+                <Route
+                    path="/admin"
+                    render={(routerProps) => {
+                        return <App {...routerProps} />
+                    }}
+                />
+                {
+                    mainRoutes.map(route => {
+                        return (
+                            <Route
+                                key={route.pathname}
+                                path={route.pathname}
+                                render={routerProps => {
+                                    return <route.component {...routerProps} />
+                                }}
+                            />
+                        )
+                    })
+                }
+                <Redirect to="/admin" from="/" exact />
+                <Redirect to="/404" />
+            </Switch>
+        </Router>
+    </ConfigProvider>,
     document.getElementById('root')
 )
